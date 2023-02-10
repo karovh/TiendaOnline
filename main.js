03ad5fef36f468e41de5db38120088aff5eac440
karovh <carolinavillamilh@gmail.com>
2023-02-10 16:59:18 -0500

    Integrando el carrito de compras al menú, ocultando un menú al abrir otro

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-const menuHamIcon = document.querySelector('.menu')
-const mobileMenu = document.querySelector('.mobile-menu')
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-card');
+const aside = document.querySelector('.product-detail');
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
-menuHamIcon.addEventListener('click', toggleMobileMenu)
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive')
+    //ocultando el menu al abrir el carrito
+    const isAsideClose = aside.classList.contains('inactive');
+    if (!isAsideClose){
+        aside.classList.add('inactive')
+    }
+    desktopMenu.classList.toggle('inactive');
 }
+
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive')
+    //ocultando el menu hamburguesa al abrir el carrito
+    const isAsideClose = aside.classList.contains('inactive');    
+    if (!isAsideClose) {
+        aside.classList.add('inactive')
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+     //ocultando el carrito al abrir el menu hamburguesa
+    const isMobileMenuClose = mobileMenu.classList.contains('inactive');    
+
+    if (!isMobileMenuClose) {
+        mobileMenu.classList.add('inactive')
+    }
+    aside.classList.toggle('inactive')    
 }
